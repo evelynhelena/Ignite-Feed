f6e1dc4db511d9245d41841ca34a9a4990780f04
evelynhelena <evelynhelena781@gmail.com>
2022-10-17 08:36:12 -0300

    criando sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,26 @@ import { Header } from "./components/Header";
 import { Post } from "./Post";
 import "./global.css";
 
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Evelyn Helena"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum modi optio minima maxime facere pariatur repellat doloremque odit neque dolore debitis aliquam sunt, voluptas quisquam ad rerum perspiciatis magni labore?"
-      />
-      <Post
-        author="Samia Helena"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum modi optio minima maxime facere pariatur repellat doloremque odit neque dolore debitis aliquam sunt, voluptas quisquam ad rerum perspiciatis magni labore?"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="Evelyn Helena"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum modi optio minima maxime facere pariatur repellat doloremque odit neque dolore debitis aliquam sunt, voluptas quisquam ad rerum perspiciatis magni labore?"
+          />
+          <Post
+            author="Samia Helena"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum modi optio minima maxime facere pariatur repellat doloremque odit neque dolore debitis aliquam sunt, voluptas quisquam ad rerum perspiciatis magni labore?"
+          />
+        </main>
+      </div>
     </>
   );
 }
