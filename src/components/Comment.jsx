c3026a32ef8822039c8ee14de6be0920833ec5b7
evelynhelena <evelynhelena781@gmail.com>
2022-10-19 16:44:15 -0300

    Finalizando layout

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,22 +1,23 @@
 import styles from "./Comment.module.css";
 import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/evelynhelena.png" />
+      <Avatar hasBorder={false} src="https://github.com/evelynhelena.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
-            <div className={styles.aythorAndTime}>
+            <div className={styles.authorAndTime}>
               <strong>Evelyn Helena</strong>
               <time title="11 de maio às 08:13" dateTime="2022-05-11 08:13:00">
                 Cerca de 1h atrás
               </time>
             </div>
             <button title="Deletar Comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
           <p>Muito bom Devon, parabéns!! 👏👏</p>
